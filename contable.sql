28de49087e7aac5336850fa92f26fbab29bd8432
manueldelapenna <manueldelapenna@gmail.com>
2017-03-03 22:06:02 -0300

    nuevo backup de bbdd

--- contable.sql
+++ contable.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-12-2016 a las 22:02:37
+-- Tiempo de generación: 04-03-2017 a las 02:05:17
 -- Versión del servidor: 10.1.13-MariaDB
 -- Versión de PHP: 7.0.8
 
@@ -19,6 +19,41 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `contable`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `account`
+--
+
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL,
+  `balance` decimal(12,4) NOT NULL,
+  `customer_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `account`
+--
+
+INSERT INTO `account` (`id`, `balance`, `customer_id`) VALUES
+(1, '0.0000', 1004),
+(2, '0.0000', 1003),
+(3, '0.0000', 1002);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `account_movement`
+--
+
+CREATE TABLE `account_movement` (
+  `id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `detail` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `amount` decimal(12,4) NOT NULL,
+  `account_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -73,7 +108,11 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`id`, `iva_condition_id`, `created_at`, `name`, `cuit_dni`, `address`, `city`, `state`, `zipcode`, `phones`, `email`, `observations`) VALUES
-(1001, 5, '2016-11-30 00:00:00', 'Consumidor Final', '00000000', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+(1001, 5, '2016-11-30 00:00:00', 'Consumidor Final', '00000000', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(1002, 5, '2016-12-29 00:00:00', 'Manuel De la Penna', '31042332', 'Calle 12 1091', 'La Plata', 'Buenos Airtes', '1900', '2983565189', 'manueldelapenna@gmail.com', NULL),
+(1003, 5, '2016-12-29 00:00:00', 'Leo Dela', '34138678', 'Calle 527', 'Tolosa', 'Buenos Aires', '1900', '2983565190', 'leodelapenna@gmail.com', NULL),
+(1004, 5, '2017-02-15 00:00:00', 'Simon', '35246807', NULL, 'La Plata', 'Buenos Aires', '1900', '2983565191', 'simondelapenna@gmail.com', NULL),
+(1005, 5, '2017-02-15 00:00:00', 'Pedro De la Penna', '12345789', NULL, 'La Plata', 'Buenos Aires', '1900', '2983565191', 'pedrito', NULL);
 
 -- --------------------------------------------------------
 
@@ -103,7 +142,62 @@ CREATE TABLE `fos_user` (
 --
 
 INSERT INTO `fos_user` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `confirmation_token`, `password_requested_at`, `roles`, `firstname`, `lastname`) VALUES
-(1, 'manueldelapenna', 'manueldelapenna', 'manueldelapenna@gmail.com', 'manueldelapenna@gmail.com', 1, 'QkedtvdReupikUbtjF5SelPxci.7EuFFA1inkmzWc.0', 'q4dFJXAKE1tRCEz/ksg7fGYuPS7K8DvqegOuELGIL0BPdsU5EcFbdZyirKt/m/Xoap5Nv8BFg2VKGXwMo0eG9w==', '2016-12-26 19:41:19', NULL, NULL, 'a:2:{i:0;s:10:"ROLE_ADMIN";i:1;s:16:"ROLE_SUPER_ADMIN";}', 'Manuel', 'De la Penna');
+(1, 'manueldelapenna', 'manueldelapenna', 'manueldelapenna@gmail.com', 'manueldelapenna@gmail.com', 1, 'QkedtvdReupikUbtjF5SelPxci.7EuFFA1inkmzWc.0', 'q4dFJXAKE1tRCEz/ksg7fGYuPS7K8DvqegOuELGIL0BPdsU5EcFbdZyirKt/m/Xoap5Nv8BFg2VKGXwMo0eG9w==', '2017-02-04 02:20:00', NULL, NULL, 'a:2:{i:0;s:10:"ROLE_ADMIN";i:1;s:16:"ROLE_SUPER_ADMIN";}', 'Manuel', 'De la Penna');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `invoice`
+--
+
+CREATE TABLE `invoice` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `sales_point_id` int(11) NOT NULL,
+  `sales_condition_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `subtotal` decimal(12,4) NOT NULL,
+  `discount_amount` decimal(12,4) NOT NULL,
+  `shipping_amount` decimal(12,4) NOT NULL,
+  `total` decimal(12,4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `invoice`
+--
+
+INSERT INTO `invoice` (`id`, `customer_id`, `sales_point_id`, `sales_condition_id`, `order_id`, `date`, `subtotal`, `discount_amount`, `shipping_amount`, `total`) VALUES
+(1, 1003, 2, 1, 43, '2017-02-15 16:16:17', '1338.0600', '5.0000', '110.0000', '1443.0600'),
+(2, 1003, 2, 1, 46, '2017-02-22 01:46:47', '1225.0500', '0.0000', '0.0000', '1225.0500'),
+(3, 1002, 2, 1, 47, '2017-02-22 16:20:38', '12070.0000', '0.0000', '0.0000', '12070.0000');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `invoice_item`
+--
+
+CREATE TABLE `invoice_item` (
+  `id` int(11) NOT NULL,
+  `invoice_id` int(11) DEFAULT NULL,
+  `product_code` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `product_quantity` double NOT NULL,
+  `product_description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `unit_price` decimal(12,4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `invoice_item`
+--
+
+INSERT INTO `invoice_item` (`id`, `invoice_id`, `product_code`, `product_quantity`, `product_description`, `unit_price`) VALUES
+(1, 1, '1234', 6, 'sdfsdf', '23.0101'),
+(2, 1, '222', 50, 'dsfsdf', '24.0000'),
+(3, 2, '123', 15, 'svsdf', '1.6700'),
+(4, 2, 'lij', 300, 'ijoij', '4.0000'),
+(5, 3, '1130', 1, 'mouse genius', '70.0000'),
+(6, 3, '1234', 1, 'notebook dell', '12000.0000');
 
 -- --------------------------------------------------------
 
@@ -152,8 +246,30 @@ CREATE TABLE `order_item` (
   `product_code` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `product_quantity` double NOT NULL,
   `product_description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `unit_price` double NOT NULL
+  `unit_price` decimal(12,4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `order_item`
+--
+
+INSERT INTO `order_item` (`id`, `order_id`, `product_code`, `product_quantity`, `product_description`, `unit_price`) VALUES
+(77, 43, '1234', 6, 'sdfsdf', '23.0101'),
+(79, 45, '123', 2, 'sdfsf', '100.0000'),
+(80, 45, '23', 5, 'sfsdf', '13.0000'),
+(81, 46, '123', 15, 'svsdf', '1.6700'),
+(82, 46, 'lij', 300, 'ijoij', '4.0000'),
+(83, 43, '222', 50, 'dsfsdf', '24.0000'),
+(84, 47, '1130', 1, 'mouse genius', '70.0000'),
+(85, 47, '1234', 1, 'notebook dell', '12000.0000'),
+(86, 48, '1234', 1, 'notebook dell', '12000.0000'),
+(87, 48, '1130', 1, 'mouse genius', '70.0000'),
+(88, 48, '000', 1, 'tarifa plana', '50.0000'),
+(89, 49, '1234', 2, 'notebook dell', '12000.0000'),
+(90, 49, '1131', 3, 'mouse genius wireless', '170.0000'),
+(91, 49, '1122', 1, 'teclado genius', '120.0000'),
+(96, 52, '1234', 1, 'notebook dell', '12000.0000'),
+(97, 52, '1122', 1, 'teclado genius', '120.0000');
 
 -- --------------------------------------------------------
 
@@ -165,6 +281,15 @@ CREATE TABLE `order_state` (
   `id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `order_state`
+--
+
+INSERT INTO `order_state` (`id`, `name`) VALUES
+(1, 'Abierto'),
+(3, 'Cancelado'),
+(2, 'Cerrado');
 
 -- --------------------------------------------------------
 
@@ -181,11 +306,21 @@ CREATE TABLE `product` (
   `provider_code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `price` double NOT NULL,
+  `price` decimal(12,4) NOT NULL,
   `stock` double NOT NULL,
   `min_stock` double NOT NULL,
   `max_stock` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `product`
+--
+
+INSERT INTO `product` (`id`, `category_id`, `provider_id`, `tax_id`, `code`, `provider_code`, `name`, `description`, `price`, `stock`, `min_stock`, `max_stock`) VALUES
+(1, 1, 1, 1, '1234', '12345', 'notebook dell', 'notebook dell i7', '12000.0000', 21, 5, 50),
+(2, 1, 1, 1, '1122', '15432', 'teclado genius', 'teclado genius ergonomico', '120.0000', 23, 5, 50),
+(3, 1, 1, 1, '1130', '15444', 'mouse genius', 'mouse genius ergonomico', '70.0000', 14, 5, 50),
+(5, 1, 1, 1, '1131', '15445', 'mouse genius wireless', 'mouse genius compatible con todos los estandares', '170.0000', 14, 5, 40);
 
 -- --------------------------------------------------------
 
@@ -206,6 +341,13 @@ CREATE TABLE `provider` (
   `observations` longtext COLLATE utf8_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `provider`
+--
+
+INSERT INTO `provider` (`id`, `name`, `email`, `cuit`, `phone`, `address`, `zipcode`, `city`, `state`, `observations`) VALUES
+(1, 'Proveedor 1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -214,15 +356,47 @@ CREATE TABLE `provider` (
 
 CREATE TABLE `purchase_order` (
   `id` int(11) NOT NULL,
-  `customer_id` int(11) DEFAULT NULL,
-  `order_state_id` int(11) DEFAULT NULL,
-  `sales_point_id` int(11) DEFAULT NULL,
+  `customer_id` int(11) NOT NULL,
+  `order_state_id` int(11) NOT NULL,
+  `sales_point_id` int(11) NOT NULL,
   `date` datetime NOT NULL,
-  `subtotal` double NOT NULL,
-  `discount_amount` double NOT NULL,
-  `shipping_amount` double NOT NULL,
-  `total` double NOT NULL
+  `subtotal` decimal(12,4) NOT NULL,
+  `discount_amount` decimal(12,4) NOT NULL,
+  `shipping_amount` decimal(12,4) NOT NULL,
+  `total` decimal(12,4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `purchase_order`
+--
+
+INSERT INTO `purchase_order` (`id`, `customer_id`, `order_state_id`, `sales_point_id`, `date`, `subtotal`, `discount_amount`, `shipping_amount`, `total`) VALUES
+(43, 1003, 2, 2, '2017-01-17 09:03:25', '1338.0600', '5.0000', '110.0000', '1443.0600'),
+(45, 1003, 2, 2, '2017-01-17 09:07:58', '265.0000', '1.0000', '20.0000', '284.0000'),
+(46, 1003, 2, 2, '2017-01-17 09:42:24', '1225.0500', '0.0000', '0.0000', '1225.0500'),
+(47, 1002, 2, 2, '2017-02-04 09:00:47', '12070.0000', '0.0000', '0.0000', '12070.0000'),
+(48, 1002, 1, 2, '2017-02-04 09:03:29', '12120.0000', '0.0000', '0.0000', '12120.0000'),
+(49, 1002, 1, 2, '2017-02-04 09:15:15', '24630.0000', '0.0000', '0.0000', '24630.0000'),
+(52, 1002, 1, 2, '2017-02-04 09:22:05', '12120.0000', '0.0000', '0.0000', '12120.0000');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sales_condition`
+--
+
+CREATE TABLE `sales_condition` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `sales_condition`
+--
+
+INSERT INTO `sales_condition` (`id`, `name`) VALUES
+(1, 'Contado'),
+(2, 'Cuenta Corriente');
 
 -- --------------------------------------------------------
 
@@ -234,6 +408,14 @@ CREATE TABLE `sales_point` (
   `id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `sales_point`
+--
+
+INSERT INTO `sales_point` (`id`, `name`) VALUES
+(2, 'Fabrica'),
+(1, 'Local');
 
 -- --------------------------------------------------------
 
@@ -248,8 +430,30 @@ CREATE TABLE `tax` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
+-- Volcado de datos para la tabla `tax`
+--
+
+INSERT INTO `tax` (`id`, `name`, `amount`) VALUES
+(1, '21.00 %', 0.21),
+(2, '10.50 %', 0.105);
+
+--
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_7D3656A49395C3F3` (`customer_id`);
+
+--
+-- Indices de la tabla `account_movement`
+--
+ALTER TABLE `account_movement`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_8C1377419B6B5FBA` (`account_id`);
 
 --
 -- Indices de la tabla `category`
@@ -274,6 +478,23 @@ ALTER TABLE `fos_user`
   ADD UNIQUE KEY `UNIQ_957A647992FC23A8` (`username_canonical`),
   ADD UNIQUE KEY `UNIQ_957A6479A0D96FBF` (`email_canonical`),
   ADD UNIQUE KEY `UNIQ_957A6479C05FB297` (`confirmation_token`);
+
+--
+-- Indices de la tabla `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_906517448D9F6D38` (`order_id`),
+  ADD KEY `IDX_906517449395C3F3` (`customer_id`),
+  ADD KEY `IDX_906517448D945686` (`sales_point_id`),
+  ADD KEY `IDX_90651744143A9E0E` (`sales_condition_id`);
+
+--
+-- Indices de la tabla `invoice_item`
+--
+ALTER TABLE `invoice_item`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_1DDE477B2989F1FD` (`invoice_id`);
 
 --
 -- Indices de la tabla `iva_condition`
@@ -326,9 +547,16 @@ ALTER TABLE `provider`
 --
 ALTER TABLE `purchase_order`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `IDX_21E210B29395C3F3` (`customer_id`),
   ADD KEY `IDX_21E210B2E420DE70` (`order_state_id`),
-  ADD KEY `IDX_21E210B28D945686` (`sales_point_id`);
+  ADD KEY `IDX_21E210B28D945686` (`sales_point_id`),
+  ADD KEY `IDX_21E210B29395C3F3` (`customer_id`);
+
+--
+-- Indices de la tabla `sales_condition`
+--
+ALTER TABLE `sales_condition`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_8198D00A5E237E06` (`name`);
 
 --
 -- Indices de la tabla `sales_point`
@@ -349,6 +577,16 @@ ALTER TABLE `tax`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `account`
+--
+ALTER TABLE `account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `account_movement`
+--
+ALTER TABLE `account_movement`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT de la tabla `category`
 --
 ALTER TABLE `category`
@@ -357,12 +595,22 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT de la tabla `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1002;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1006;
 --
 -- AUTO_INCREMENT de la tabla `fos_user`
 --
 ALTER TABLE `fos_user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `invoice`
+--
+ALTER TABLE `invoice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT de la tabla `invoice_item`
+--
+ALTER TABLE `invoice_item`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT de la tabla `iva_condition`
 --
@@ -377,40 +625,57 @@ ALTER TABLE `order_comment`
 -- AUTO_INCREMENT de la tabla `order_item`
 --
 ALTER TABLE `order_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
 --
 -- AUTO_INCREMENT de la tabla `order_state`
 --
 ALTER TABLE `order_state`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT de la tabla `provider`
 --
 ALTER TABLE `provider`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `purchase_order`
 --
 ALTER TABLE `purchase_order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+--
+-- AUTO_INCREMENT de la tabla `sales_condition`
+--
+ALTER TABLE `sales_condition`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `sales_point`
 --
 ALTER TABLE `sales_point`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `tax`
 --
 ALTER TABLE `tax`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `account`
+--
+ALTER TABLE `account`
+  ADD CONSTRAINT `FK_7D3656A49395C3F3` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+
+--
+-- Filtros para la tabla `account_movement`
+--
+ALTER TABLE `account_movement`
+  ADD CONSTRAINT `FK_8C1377419B6B5FBA` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`);
 
 --
 -- Filtros para la tabla `category`
@@ -425,6 +690,21 @@ ALTER TABLE `customer`
   ADD CONSTRAINT `FK_81398E09E0AD1F90` FOREIGN KEY (`iva_condition_id`) REFERENCES `iva_condition` (`id`);
 
 --
+-- Filtros para la tabla `invoice`
+--
+ALTER TABLE `invoice`
+  ADD CONSTRAINT `FK_90651744143A9E0E` FOREIGN KEY (`sales_condition_id`) REFERENCES `sales_condition` (`id`),
+  ADD CONSTRAINT `FK_906517448D945686` FOREIGN KEY (`sales_point_id`) REFERENCES `sales_point` (`id`),
+  ADD CONSTRAINT `FK_906517448D9F6D38` FOREIGN KEY (`order_id`) REFERENCES `purchase_order` (`id`),
+  ADD CONSTRAINT `FK_906517449395C3F3` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+
+--
+-- Filtros para la tabla `invoice_item`
+--
+ALTER TABLE `invoice_item`
+  ADD CONSTRAINT `FK_1DDE477B2989F1FD` FOREIGN KEY (`invoice_id`) REFERENCES `invoice` (`id`) ON DELETE CASCADE;
+
+--
 -- Filtros para la tabla `order_comment`
 --
 ALTER TABLE `order_comment`
@@ -434,7 +714,7 @@ ALTER TABLE `order_comment`
 -- Filtros para la tabla `order_item`
 --
 ALTER TABLE `order_item`
-  ADD CONSTRAINT `FK_52EA1F098D9F6D38` FOREIGN KEY (`order_id`) REFERENCES `purchase_order` (`id`);
+  ADD CONSTRAINT `FK_52EA1F098D9F6D38` FOREIGN KEY (`order_id`) REFERENCES `purchase_order` (`id`) ON DELETE CASCADE;
 
 --
 -- Filtros para la tabla `product`
