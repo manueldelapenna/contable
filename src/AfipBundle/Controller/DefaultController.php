18a0ec65dc2be708a037e6353648916ad8b2dc0c
manueldelapenna <manueldelapenna@gmail.com>
2017-04-01 18:13:56 -0300

    refactoring afip

--- src/AfipBundle/Controller/DefaultController.php
+++ src/AfipBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AfipBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AfipBundle\Entity\Model\Environment\StagingEnvironment;
 
 class DefaultController extends Controller
 {
@@ -12,6 +13,7 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+        $environment = new StagingEnvironment();
         echo $this->container->get('kernel')->getRootDir(); die;
         return $this->render('AfipBundle:Default:index.html.twig');
     }
--- src/AfipBundle/Entity/Model/Environment/ProductionEnvironment.php
+++ src/AfipBundle/Entity/Model/Environment/ProductionEnvironment.php
@@ -1,73 +1,64 @@
 <?php
-	
-	/**
-	 * Helper for production environment of AFIP Invoice Manager.
-	 *
-	 
-	 * @author Eduardo Casey
-	 */
-	final class ProductionEnvironment extends Environment
-	{
-		/**
-		 * Returns a new instance.
-		 *
-		 * @return ProductionEnvironment instance
-		 */
-		public static function getInstance()
-		{
-			return new self();
-		}
-		
-		public function __construct()
-		{
-			parent::__construct();
-		}
-		
-		
-		public function getAfipCertificate()
-		{
-			return "{$this->getCertificatesDirectoryPath()}/AFIPcerthomo.crt";
-		}
-		
-		public function getConsumerCertificate()
-		{
-			return "{$this->getCertificatesDirectoryPath()}/usershop_prod.crt";
-		}
-		
-		public function getConsumerPrivateKey()
-		{
-			return "{$this->getCertificatesDirectoryPath()}/private_prod.key";
-		}
-		
-		public function getEnvironmentKey()
-		{
-			return "production";
-		}
-		
-		public function getPointOfSale()
-		{
-			return "0006";
-		}
-		
-		public function getTaxpayerCuit()
-		{
-			return (float) "33709315329";
-		}
-		
-		public function getWsaaCommonName()
-		{
-			return "wsaa.afip.gov.ar";
-		}
-		
-		public function getWsaaUrl()
-		{
-			return "https://wsaa.afip.gov.ar/ws/services/LoginCms";
-		}
-		
-		public function getWsfeUrl()
-		{
-			return "https://servicios1.afip.gov.ar/wsfev1/service.asmx";
-		}
-	}
-	
+
+namespace AfipBundle\Entity\Model\Environment;
+
+/**
+ * Helper for production environment of AFIP Invoice Manager.
+ *
+
+ * @author Eduardo Casey
+ */
+final class ProductionEnvironment extends Environment {
+
+    /**
+     * Returns a new instance.
+     *
+     * @return ProductionEnvironment instance
+     */
+    public static function getInstance() {
+        return new self();
+    }
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function getAfipCertificate() {
+        return "{$this->getCertificatesDirectoryPath()}/AFIPcerthomo.crt";
+    }
+
+    public function getConsumerCertificate() {
+        return "{$this->getCertificatesDirectoryPath()}/usershop_prod.crt";
+    }
+
+    public function getConsumerPrivateKey() {
+        return "{$this->getCertificatesDirectoryPath()}/private_prod.key";
+    }
+
+    public function getEnvironmentKey() {
+        return "production";
+    }
+
+    public function getPointOfSale() {
+        return "0006";
+    }
+
+    public function getTaxpayerCuit() {
+        return (float) "33709315329";
+    }
+
+    public function getWsaaCommonName() {
+        return "wsaa.afip.gov.ar";
+    }
+
+    public function getWsaaUrl() {
+        return "https://wsaa.afip.gov.ar/ws/services/LoginCms";
+    }
+
+    public function getWsfeUrl() {
+        return "https://servicios1.afip.gov.ar/wsfev1/service.asmx";
+    }
+
+}
+
 ?>
