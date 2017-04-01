95fe76ec5e5f8ff4da2c48b47574f03abb25d9c2
manueldelapenna <manueldelapenna@gmail.com>
2017-04-01 17:41:32 -0300

    refactoring afip

--- src/AfipBundle/Entity/Model/BillingTarget/BillingTargetForCreditNote.php
+++ src/AfipBundle/Entity/Model/BillingTarget/BillingTargetForCreditNote.php
@@ -1,40 +1,36 @@
 <?php
-	
-	/**
-	 * Billing target for Credit Notes.
-	 *
-	 
-	 * @author Eduardo Casey
-	 */
-	class BillingTargetForCreditNote extends BillingTarget
-	{
-		/* Public methods */
-		
-		public function getInstance()
-		{
-			$target = new self();
-			return $target;
-		}
-		
-		public function __construct()
-		{
-			$this->initialize();
-		}
-		
-		
-		
-		/* Protected methods */
-		
-		protected function getNameForPeople()
-		{
-			return "credit note";
-		}
-		
-		protected function getValidTargetTypes()
-		{
-			$types = TypeEnum::getTypesForCreditNote();
-			return $types;
-		}
-	}
-	
+
+namespace AfipBundle\Entity\Model\BillingTarget;
+
+/**
+ * Billing target for Credit Notes.
+ *
+
+ * @author Eduardo Casey
+ */
+class BillingTargetForCreditNote extends BillingTarget {
+    /* Public methods */
+
+    public function getInstance() {
+        $target = new self();
+        return $target;
+    }
+
+    public function __construct() {
+        $this->initialize();
+    }
+
+    /* Protected methods */
+
+    protected function getNameForPeople() {
+        return "credit note";
+    }
+
+    protected function getValidTargetTypes() {
+        $types = TypeEnum::getTypesForCreditNote();
+        return $types;
+    }
+
+}
+
 ?>
--- src/AfipBundle/Entity/Model/BillingTarget/BillingTargetForDebitNote.php
+++ src/AfipBundle/Entity/Model/BillingTarget/BillingTargetForDebitNote.php
@@ -1,40 +1,36 @@
 <?php
-	
-	/**
-	 * Billing target for Debit Notes.
-	 *
-	 
-	 * @author Eduardo Casey
-	 */
-	class BillingTargetForDebitNote extends BillingTarget
-	{
-		/* Public methods */
-		
-		public function getInstance()
-		{
-			$target = new self();
-			return $target;
-		}
-		
-		public function __construct()
-		{
-			$this->initialize();
-		}
-		
-		
-		
-		/* Protected methods */
-		
-		protected function getNameForPeople()
-		{
-			return "debit note";
-		}
-		
-		protected function getValidTargetTypes()
-		{
-			$types = TypeEnum::getTypesForDebitNote();
-			return $types;
-		}
-	}
-	
+
+namespace AfipBundle\Entity\Model\BillingTarget;
+
+/**
+ * Billing target for Debit Notes.
+ *
+
+ * @author Eduardo Casey
+ */
+class BillingTargetForDebitNote extends BillingTarget {
+    /* Public methods */
+
+    public function getInstance() {
+        $target = new self();
+        return $target;
+    }
+
+    public function __construct() {
+        $this->initialize();
+    }
+
+    /* Protected methods */
+
+    protected function getNameForPeople() {
+        return "debit note";
+    }
+
+    protected function getValidTargetTypes() {
+        $types = TypeEnum::getTypesForDebitNote();
+        return $types;
+    }
+
+}
+
 ?>
